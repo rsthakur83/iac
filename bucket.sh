cd45623333e3e086cb2f5c18d00cfe00be4bc7dd
root <root@training-ravinder-2.c.puls-xilium.internal>
2021-09-29 10:44:48 +0000

    first commit

--- bucket.sh
+++ bucket.sh
@@ -1,5 +1,5 @@
-mkdir /root/.aws/
-echo "[default]"  >> /root/.aws/credentials
+sudo mkdir /root/.aws/
+sudo echo "[default]"  >> /root/.aws/credentials
 ./terraform plan -out=/tmp/plan.out
 ./terraform show -no-color -json /tmp/plan.out > /tmp/test.json
 ./terraform apply --auto-approve
