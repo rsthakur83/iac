faa420929b7cf03f454e76ff71b19a727a4b6100
root <root@training-ravinder-2.c.puls-xilium.internal>
2021-09-29 10:43:39 +0000

    first commit

--- install.sh
+++ install.sh
@@ -2,5 +2,4 @@ wget https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux_amd6
 apt-get update; apt-get install awscli unzip -y
 unzip terraform_0.14.0_linux_amd64.zip
 chmod +x terraform
-cp terraform /bin
 pip3 install -r requirements.txt
