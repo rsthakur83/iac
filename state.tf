71f243d660be9ee21271fc749191e72d588f27b1
root <root@training-ravinder-2.c.puls-xilium.internal>
2021-09-29 12:01:57 +0000

    first commit

--- state.tf
+++ state.tf
@@ -1,13 +1,15 @@
-
 provider "aws" {
   region = "us-east-1"
 }
+
+
 
 
 terraform {
   backend "s3" {
     bucket = "terraform-state-bucket-iac"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "aws-region"
   }
 }
+
