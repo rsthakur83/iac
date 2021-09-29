6b7c60f2a1ac738eb8ab8124dde735e6651bae74
root <root@training-ravinder-2.c.puls-xilium.internal>
2021-09-29 11:18:26 +0000

    first commit

--- bucket.sh
+++ bucket.sh
@@ -1,7 +1,7 @@
-#wget https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux_amd64.zip
+wget https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux_amd64.zip
 #apt-get update; apt-get install awscli unzip -y
-#unzip terraform_0.14.0_linux_amd64.zip
-#chmod +x terraform
+unzip terraform_0.14.0_linux_amd64.zip
+chmod +x terraform
 #pip3 install -r requirements.txt
 #sudo mkdir /root/.aws/
 ./terraform init
