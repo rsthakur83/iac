5e44c46bc410ab442d82d381208bcb6aeba3b4cd
root <root@training-ravinder-2.c.puls-xilium.internal>
2021-09-29 10:56:09 +0000

    first commit

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ sudo wget https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux
 sudo apt-get update; apt-get install awscli unzip -y
 sudo unzip terraform_0.14.0_linux_amd64.zip
 chmod +x terraform
-cd backend;../terraform init;./terraform plan;terraform apply --auto-approve
+cd backend;../terraform init;../terraform plan;../terraform apply --auto-approve
 pip3 install -r requirements.txt
