6931c86db534521f2de3c950545172bb6f499743
rsthakur83 <rsthakur83@yahoo.com>
2021-10-05 11:55:45 +0200

    Update test_integration.py

--- integration/test_integration.py
+++ integration/test_integration.py
@@ -7,4 +7,4 @@ import json
 @terraform('aws_s3', scope='session')
 def test_s3_copy(aws_s3):
     s3cp = Session().client('s3')
-    res =s3cp.put_object(Body="/root/terraform-test/s3.tf", Bucket="cloudnewbucketforyouiac", Key="terraformfile")
+    res =s3cp.put_object(Body="/root/terraform-test/s3.tf", Bucket="cloudnewbucketforyouiac-newone", Key="terraformfile")
